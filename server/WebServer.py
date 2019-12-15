12dbba38e2ea96dbaf673e9ac36d79a211bcbabc
EduardoRenani <eduardo.renani@hotmail.com>
2019-12-15 16:26:00 -0300

    creating multiclient connection handler on server side

--- server/WebServer.py
+++ server/WebServer.py
@@ -1,8 +1,10 @@
 #Import socket module
 from socket import *
 import sys 
-import time
+import os
+import time, datetime
 import threading
+import csv
 
 PAYLOAD_SIZE = 4096 #bytes
 TIME_WINDOW = 2 #seconds
@@ -18,15 +20,39 @@ class Timer (threading.Thread):
         self.payloadSize = payloadSize
       
     def run(self):
+        with open('log.csv', 'w') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"')
+            try: 
+                while 1:
+                    initialBufferSize = self.payloadSize.size
+                    time.sleep(self.delay)
+                    afterBufferSize = self.payloadSize.size
+
+                    amountOfData = afterBufferSize - initialBufferSize
+                    megaBitsAmountOfData = (amountOfData*8)/(10**6)
+                    transferRate = megaBitsAmountOfData/self.delay
+
+                    writer.writerow([os.getpid(), datetime.datetime.now().isoformat(), transferRate])
+                    file.flush()
+                    print('Transfer rate: ', transferRate, 'Mbits/s')
+            
+            except KeyboardInterrupt:
+                file.close()
+                print("Server stopped")
+
+class ConnectionHandler(threading.Thread):
+    def __init__(self, newConnectionSocket, addr):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.name = addr
+
+    def run(self):
+        payloadSize = PayloadSize(0)
+        timer = Timer(TIME_WINDOW, payloadSize)
+        timer.start()
         while 1:
-            initialBufferSize = self.payloadSize.size
-            time.sleep(self.delay)
-            afterBufferSize = self.payloadSize.size
-
-            amountOfData = afterBufferSize - initialBufferSize
-            megaBitsAmountOfData = (amountOfData*8)/(10**6)
-            print('Transfer rate: ', megaBitsAmountOfData/self.delay, 'Mbits/s')
-
+            payload = connectionSocket.recv(PAYLOAD_SIZE)
+            payloadSize.size = payloadSize.size + sys.getsizeof(payload)
 
 
 
@@ -44,7 +70,7 @@ serverPort = int(sys.argv[1])
 # binds the socket to given port.
 serverSocket.bind(('', serverPort))
 # starts to listen for SYNACK 
-serverSocket.listen(1)
+serverSocket.listen(10)
 
 print('The Server is ready to receive')
 
@@ -52,10 +78,6 @@ while 1:
     # Accepts new connections creating connection sockets 
     connectionSocket, addr = serverSocket.accept()
     
-    # reads payload data
-    payloadSize = PayloadSize(0)
-    timer = Timer(TIME_WINDOW, payloadSize)
-    timer.start()
-    while 1:
-        payload = connectionSocket.recv(PAYLOAD_SIZE)
-        payloadSize.size = payloadSize.size + sys.getsizeof(payload)
+    # create new thread to handler different connections
+    connectionHandler = ConnectionHandler(connectionSocket, addr)
+    connectionHandler.start()
